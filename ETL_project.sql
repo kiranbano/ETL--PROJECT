588f0857a61d1bf64559b33b71f341a15023e64f
kiranbano <72603532+kiranbano@users.noreply.github.com>
2021-05-08 10:15:57 +1000

    Delete ETL_project.sql

--- ETL_project.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS hydro_2019;
-CREATE TABLE hydro_2019 (
-  id serial Not NULL,
-  postcode varchar not NULL,
-  Installations Quantity Total int not NULL;
-  
-  
---) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-select * from postcodes_geo;
